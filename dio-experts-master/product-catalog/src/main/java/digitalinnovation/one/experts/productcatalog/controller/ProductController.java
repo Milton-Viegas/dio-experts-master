0046e045a416c3a71dfe63c16a58c61976a1515b
Viegas <milton.viegas.br@gmail.com>
2021-08-06 10:12:47 -0300

    "Update elasticsearch version" :wq

--- dio-experts-master/product-catalog/src/main/java/digitalinnovation/one/experts/productcatalog/controller/ProductController.java
+++ dio-experts-master/product-catalog/src/main/java/digitalinnovation/one/experts/productcatalog/controller/ProductController.java
@@ -15,6 +15,24 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @RequestMapping(method = RequestMethod.POST)
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
 
+    @RequestMapping
+    public Iterable<Product> list() {
+        return productRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}")
+    public Optional<Product> findById(@PathVariable("id") Integer id) {
+        return productRepository.findById(id);
+    }
+
+    @RequestMapping(value = "/name/{name}")
+    public Iterable<Product> findByName(@PathVariable("name") String name) {
+        return productRepository.findByName(name);
+    }
 
 }
--- dio-experts-master/run-elasticsearch.sh
+++ dio-experts-master/run-elasticsearch.sh
@@ -1,1 +1,1 @@
-docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:6.6.2
+docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.5.2
